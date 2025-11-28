cf24a739e3ce1171de2ff772c0f30cf67b235616
Eisenbarth Martin <EISENBARTH.Martin@htleisenstadt.at>
2025-11-28 11:38:30 +0100

    Code Kommentiert

--- PI.c
+++ PI.c
@@ -2,12 +2,13 @@
 #include <math.h>
 
 int main() {
-    int n = 6;            // Start mit einem Sechseck
+    int n = 6;            // Start mit einem Sechseck/n=Anzahl der Ecken 
     double pi_approx;       //Double speichert dezimalzahl
 
     for (; n <= 1000000; n *= 2) {  // Seitenzahl verdoppeln
         pi_approx = n * tan(M_PI / n);  // Inkreis-Formel
-        printf("n = %d -> pi ? %.15f\n", n, pi_approx);
+        printf("n = %d -> pi ? %.15f\n", n, pi_approx);     //%d gibt die Ganzzahl n aus.
+                                                            // % .15f gibt die Pi - Näherung mit 15 Nachkommastellen aus.
     }
 
     return 0;
