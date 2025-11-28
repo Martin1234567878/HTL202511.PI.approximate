528917940c8b1024618955c1c65ccc09ef74f780
Eisenbarth Martin <EISENBARTH.Martin@htleisenstadt.at>
2025-11-28 12:01:34 +0100

    Berechnung umgeändert

--- PI.c
+++ PI.c
@@ -2,13 +2,13 @@
 #include <math.h>
 
 int main() {
-    int n = 6;            // Start mit einem Sechseck/n=Anzahl der Ecken 
+    int n = 6;            //Start mit einem Sechseck/n=Anzahl der Ecken 
     double pi_approx;       //Double speichert dezimalzahl
 
-    for (; n <= 1000000; n *= 2) {  // Seitenzahl verdoppeln
-        pi_approx = n * tan(M_PI / n);  // Inkreis-Formel
+    for (; n <= 1000000; n *= 2) {  //Seitenzahl verdoppeln
+        pi_approx = n * tan(M_PI / n);  //Inkreis-Formel   //Berechnung der Pi-Näherung
         printf("n = %d -> pi ? %.15f\n", n, pi_approx);     //%d gibt die Ganzzahl n aus.
-                                                            // % .15f gibt die Pi - Näherung mit 15 Nachkommastellen aus.
+                                                            //% .15f gibt die Pi - Näherung mit 15 Nachkommastellen aus.
     }
 
     return 0;
